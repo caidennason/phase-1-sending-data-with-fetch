e77a2d7ec334af13d69e63756851ce0c8eb57096
caiden nason <caidenbnason@gmail.com>
2022-05-16 20:54:05 -0400

    commit tree 3014ee92c2d11974c87c615495ce42ca06366fd4

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email,
+        })
+
+    })
+        .then((resp) => resp.json())
+        .then((res) => 
+        {
+        let body = document.querySelector('body')
+        body.innerHTML = res.id
+        })
+   .catch((error) => {
+    let body = document.querySelector('body')
+        body.innerHTML = error.message
+   })
+        
+}
+
+
+
